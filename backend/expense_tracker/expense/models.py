ab571729cc6d48f8e5faca2de6b89d61a7191e99
Vaheed Nematpour <vaheed.nematpour@gmail.com>
2024-11-24 13:57:33 +0330

    Implement Data Modeling

--- backend/expense_tracker/expense/models.py
+++ backend/expense_tracker/expense/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Expense(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+    def __str__(self) -> str:
+        return self.title

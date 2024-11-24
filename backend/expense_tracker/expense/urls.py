7dd3ee9f1f4ff08252723eeaae6196768ce20a76
Vaheed Nematpour <vaheed.nematpour@gmail.com>
2024-11-24 14:55:34 +0330

    Implement Category detail

--- backend/expense_tracker/expense/urls.py
+++ backend/expense_tracker/expense/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     # category
     path('categories/', views.category_list),
+    path('categories/<int:id>/', views.category_detail),
 
     # expense
 ]
--- backend/expense_tracker/expense/views.py
+++ backend/expense_tracker/expense/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Category
@@ -7,5 +8,13 @@ from .serializers import CategorySerializer
 def category_list(request):
     categories = Category.objects.all()
     serializer = CategorySerializer(categories, many=True)
+
+    return Response(serializer.data)
+
+
+@api_view()
+def category_detail(request, id):
+    category = get_object_or_404(Category, pk=id)
+    serializer = CategorySerializer(category)
 
     return Response(serializer.data)

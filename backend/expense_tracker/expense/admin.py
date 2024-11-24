5959f4728f000fbe02f942fd7608309d6768e8a4
Vaheed Nematpour <vaheed.nematpour@gmail.com>
2024-11-24 14:22:38 +0330

    Implement the Admin Panel

--- backend/expense_tracker/expense/admin.py
+++ backend/expense_tracker/expense/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Expense
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    list_per_page = 10
+    search_fields = ['title__startswith']
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'category']
+    list_filter = ['category']
+    list_select_related = ['category']
+    list_per_page = 10
+    search_fields = ['title__startswith']

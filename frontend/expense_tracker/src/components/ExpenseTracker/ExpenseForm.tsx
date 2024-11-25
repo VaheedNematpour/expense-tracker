a2bdea256f46c4a542d5fd7b40d5416272b4c50d
Vaheed Nematpour <vaheed.nematpour@gmail.com>
2024-11-25 16:49:02 +0330

    Implemented added dependancies for managing expense form

--- frontend/expense_tracker/src/components/ExpenseTracker/ExpenseForm.tsx
+++ frontend/expense_tracker/src/components/ExpenseTracker/ExpenseForm.tsx
@@ -1,8 +1,32 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+  title: z.string().min(3),
+  description: z.string().min(3),
+  category: z
+    .number({ invalid_type_error: "The Category field is required!" })
+    .min(1),
+  amount: z
+    .number({ invalid_type_error: "The Amount field is required!" })
+    .min(0.01)
+    .max(999.99),
+});
+
+type FormData = z.infer<typeof schema>;
+
 interface Props {
   isDark: boolean;
 }
 
 function ExpenseForm({ isDark }: Props) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
   return (
     <>
       <h2
@@ -13,7 +37,12 @@ function ExpenseForm({ isDark }: Props) {
         Expense Form
       </h2>
 
-      <form className="space-y-3">
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}
+      >
         <div className="space-y-1">
           <label
             htmlFor="title"
@@ -24,11 +53,17 @@ function ExpenseForm({ isDark }: Props) {
             Title
           </label>
           <input
+            {...register("title")}
             type="text"
             id="title"
             className={`block w-full bg-gray-300 px-6 py-2 text-lg rounded xl:text-xl`}
             placeholder="Title"
           />
+          {errors.title && (
+            <p className="text-lg text-red-400 xl:text-xl">
+              {errors.title.message}
+            </p>
+          )}
         </div>
 
         <div className="space-y-1">
@@ -41,9 +76,42 @@ function ExpenseForm({ isDark }: Props) {
             Description
           </label>
           <textarea
+            {...register("description")}
             placeholder="description"
             className={`block w-full bg-gray-300 px-6 py-2 text-lg rounded xl:text-xl`}
           ></textarea>
+          {errors.description && (
+            <p className="text-lg text-red-400 xl:text-xl">
+              {errors.description.message}
+            </p>
+          )}
+        </div>
+
+        <div className="space-y-1">
+          <label
+            htmlFor="category"
+            className={`text-lg ${
+              isDark ? "text-gray-300" : "text-gray-800"
+            } font-medium xl:text-xl`}
+          >
+            Category
+          </label>
+
+          <select
+            id="category"
+            {...register("category", { valueAsNumber: true })}
+            className={`block w-full px-6 py-1 text-lg text-gray-800 bg-gray-300 rounded`}
+          >
+            <option value=""></option>
+            <option value="1">category 1</option>
+            <option value="2">category 2</option>
+            <option value="3">category 3</option>
+          </select>
+          {errors.category && (
+            <p className="text-lg text-red-400 xl:text-xl">
+              {errors.category.message}
+            </p>
+          )}
         </div>
 
         <div className="space-y-1">
@@ -56,13 +124,27 @@ function ExpenseForm({ isDark }: Props) {
             Amount
           </label>
           <input
+            {...register("amount", { valueAsNumber: true })}
             type="number"
             placeholder="Amount"
             className={`block w-full bg-gray-300 px-6 py-2 text-lg rounded xl:text-xl`}
           />
+          {errors.amount && (
+            <p className="text-lg text-red-400 xl:text-xl">
+              {errors.amount.message}
+            </p>
+          )}
         </div>
 
-        <button>Add</button>
+        <button
+          className={`w-full py-2 border-2 ${
+            isDark
+              ? "border-gray-300 text-gray-300"
+              : "border-gray-800 text-gray-800"
+          } text-lg rounded xl:text-xl`}
+        >
+          Add
+        </button>
       </form>
     </>
   );
